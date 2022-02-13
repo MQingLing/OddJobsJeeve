d03037441812f162715d53050a6f55e2a191cb2e
Hanisah Musrin <s10226112@connect.np.edu.sg>
2022-02-13 20:54:49 +0800

    Added interactive logo and made buttons active instead of hover

--- assets/js/Homepage.js
+++ assets/js/Homepage.js
@@ -17,3 +17,23 @@ music_icon.onclick = function()
         music_icon.className = "fas fa-volume-mute";
     }
 }
+
+//logo elements
+var logo = document.querySelector(".logo img");
+var hey = document.getElementById("hey_sfx");
+
+logo.classList.toggle('wiggle');
+
+//logo interactability
+logo.onclick = function()
+{
+    hey.play();
+    logo.classList.remove('wiggle'); //if click, stop wiggle
+    logo.classList.toggle('wiggleinterrupt');
+
+    setTimeout(function() //only start wiggling again after 1.1s
+    {
+        logo.classList.remove('wiggleinterrupt');
+        logo.classList.toggle('wiggle');
+    },1100);
+}
