35f42b6505cbf94e197871f903fc7212e7607216
MQingLing <s10227446@connect.np.edu.sg>
2022-02-15 00:35:25 +0800

    added js for level and update html for the js name

---
+++ assets/js/Level.js
@@ -0,0 +1,19 @@
+//music player elements
+var waitSong = document.getElementById("waitSong");
+var music_icon = document.getElementById("music_icon");
+waitSong.loop = true;
+
+//music player interactability
+music_icon.onclick = function()
+{
+    if(waitSong.paused)
+    {
+        waitSong.play();
+        music_icon.className = "fas fa-volume-up";
+    }
+    else
+    {
+        waitSong.pause();
+        music_icon.className = "fas fa-volume-mute";
+    }
+}
