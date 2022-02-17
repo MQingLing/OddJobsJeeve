16e245f23623635427db891332263150506270f9
MQingLing <s10227446@connect.np.edu.sg>
2022-02-17 16:51:16 +0800

    update html for avatar,tutorial and ccs inculde level

---
+++ assets/js/Avatar-Selection.js
@@ -0,0 +1,10 @@
+
+//redirect to homepage page
+document.querySelector(".menu-button").onclick = function () {
+    location.href = "../html/Homepage.html";
+};
+
+//redirect to game page
+document.querySelector(".play-button").onclick = function () {
+    location.href = "../../game.html";
+};
