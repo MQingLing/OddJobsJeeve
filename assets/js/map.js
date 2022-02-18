6d96a67091f724e12b386fcbc8413d86a41f57b5
MQingLing <s10227446@connect.np.edu.sg>
2022-02-18 21:09:25 +0800

    Add in tutorial bugger

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,15 +1,4 @@
 var GAME_LEVELS = [
-	/*[
-	  "                      ",
-	  "                      ",
-	  "  x              = x  ",
-	  "  x         o o    x  ",
-	  "  x @      xxxxx   x  ",
-	  "  xxxxx            x  ",
-	  "      x!!!!!!!!!!!!x  ",
-	  "      xxxxxxxxxxxxxx  ",
-	  "                      "
-	],*/
 	[
 		"            vv                                    	  ",
 		"                          +                        = ",
@@ -38,50 +27,5 @@ var GAME_LEVELS = [
 		" @                                              	  ",
 		"xxx!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!xxx",
 		"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-	],
-	/*[
-		"                                                    ",
-		"                                                    ",
-		"                                                    ",
-		"                       ooooo                        ",
-		"                    ooxxxxxxxoo                     ",
-		"                   oxxx     xxxoo                   ",
-		"                  oxx         xxxo        o         ",
-		"                 ox       oo    xxo  o   xx         ",
-		"                 xxo     xxxo    xx  xx      o      ",
-		"                  xxo       xo               xx     ",
-		"                   xxo     @xx=           o        =",
-		"                    xx     xx    oooo    xx         ",
-		"                     xx!!xx  xxxxxxxxx              ",
-		"                      vxxv                          ",
-		"                                                    ",
-		"                                                    ",
-		"                                                    ",
-
-	],
-	[
-		"x                                                                                                        x",
-		"x @                                                                                                      x",
-		"xxxxxxxxx                                                                                               =x",
-		"x       x                                                                                                x",
-		"x       x     ooo       oo     oo   oo   oo    oo       oo                                               x",
-		"x       xxxxxxxxxxxx    xx     xx   xx   xx    xx   x   xxxxxx    oo                                     x",
-		"x                  x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!xxxxxxxxxxxxxxx     ooo                         x",
-		"x                  x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!xxxxxxxxxxxxxxxxxxxxxxxxxx       ooo            x",
-		"x                  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx          x",
-		"x                     v    v       v     v   v     v   v                                                 x",
-		"x                                                                                                        x",
-		"x                       oo     o    ooo    o   ooo   o                                                   x",
-		"x                                                                                                        x",
-		"x    o  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-		"x   o                                                                                                    x",
-		"x  o                                                                                    =                x",
-		"x o               ooo     ooo           ooo       ooo           ooo            ooo                       x",
-		"xo                                                                                                =      x",
-		"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx     x",
-		"x                                          =                                                             x",
-		"x          =         oooooooooooooooooooooooooooooooooooooooooooooooooooooo                              x",
-		"x=                                                                                                       x",
-		"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-	]*/
+	]
 ];
